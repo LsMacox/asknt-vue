fc172051b9a84a6094863668e8d9be810e5c7a70
LsMacox <zugeraev2@gmail.com>
2021-12-06 16:08:16 +0300

    [modified] build.sh

--- build.sh
+++ build.sh
@@ -7,24 +7,15 @@ if [ ! -f "$FILE_ENV" ]; then
     cp .env.example .env
 fi
 
-if [ -f .env ]; then
-    # Load Environment Variables
-    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
-fi
+export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 
-if [ -n "$BUILD_GIT_BRANCH" ]; then BUILD_GIT_BRANCH='main'; fi
+if [ -z "$BUILD_GIT_BRANCH" ]; then BUILD_GIT_BRANCH='main'; fi
 
 git fetch origin ${BUILD_GIT_BRANCH}
 git reset --hard origin/${BUILD_GIT_BRANCH}
 
-DIR_BACKEND='../asknt-laravel-back-hpace'
-DIR_NODEMODULES='node_modules'
-if [ -d "$DIR_NODEMODULES" ];
-then
-    npm update
-else
-    npm install
-fi
+yarn add node-sass
+yarn install
 
 NODE_OPTIONS=--max_old_space_size=4096 npm run build
 
